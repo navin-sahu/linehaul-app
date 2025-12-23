8fde0ce2b5b1edcf409b59dbd474a22a8da16175
navin-sahu <129816685+navin-sahu@users.noreply.github.com>
2025-12-23 06:15:04 +0000

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,12 +8,7 @@ function App() {
     <>
       <div>
         <h1>final done</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+       
       </div>
       <h1>Vite + React</h1>
       <div className="card">
