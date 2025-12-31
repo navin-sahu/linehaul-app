191e7989575b8052723a9e1d4a4efb175b9feeab
navin kumar sahu <sahunavin2@gmail.com>
2025-12-31 13:53:32 +0530

    refactor: simplify Vite configuration by removing unnecessary Babel plugins

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // ✅ REQUIRED for Vercel deep routes
-  plugins: [
-    react({
-      babel: {
-        plugins: [["babel-plugin-react-compiler"]],
-      },
-    }),
-  ],
+  base: "/",
+  plugins: [react()],
 });
