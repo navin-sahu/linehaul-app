26474ee521fcdea80b4fd4255e275e4865791f2d
navin kumar sahu <sahunavin2@gmail.com>
2026-01-09 08:29:54 +0530

    feat: add loading spinner container for improved layout

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -3,10 +3,12 @@ import "./css/LoadingSpinner.css";
 
 const LoadingSpinner = ({ size = 20 }) => {
   return (
+    <div className="loading-spinner-container">
     <AiOutlineLoading3Quarters
       className="loading-spinner"
       style={{ fontSize: size }}
     />
+    </div>
   );
 };
 
